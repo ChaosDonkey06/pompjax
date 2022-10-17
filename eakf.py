f7f5bcd8483eb56713449ddff70af7cfc6a19ea4
Jaime Cascante <je.cascante10@uniandes.edu.co>
2022-10-17 12:27:19 -0400

    update particle filter

--- eakf.py
+++ eakf.py
@@ -1,12 +1,12 @@
 import jax.numpy as np
 
-def eakf(x, y_sim, y, oev, return_gain=False):
+def eakf(x, y, z, oev, return_gain=False):
     """ Ensemble Adjustment Kalman Filter
 
     Args:
         x    : State space at time t. Shape (n: number of state variables, m: number of ensembles)
-        y_sim: Simulated observation.
-        y    : Measured observation.
+        y: Simulated observation.
+        z    : Measured observation.
         oev  : Observation error variance.
         return_gain (bool, optional): If true return the gain of the filter. Defaults to False.
 
@@ -16,20 +16,20 @@ def eakf(x, y_sim, y, oev, return_gain=False):
     """
     # num_vars, num_ens = x.shape
 
-    prior_mean_ct = y_sim.mean(-1, keepdims=True)  # Average over ensemble members
-    prior_var_ct  = y_sim.var(-1, keepdims=True)   # Compute variance over ensemble members
+    prior_mean_ct = y.mean(-1, keepdims=True)  # Average over ensemble members
+    prior_var_ct  = y.var(-1, keepdims=True)   # Compute variance over ensemble members
 
     post_var_ct   = prior_var_ct * oev / (prior_var_ct + oev)
-    post_mean_ct  = post_var_ct * (prior_mean_ct/prior_var_ct + y / oev)
+    post_mean_ct  = post_var_ct * (prior_mean_ct/prior_var_ct + z / oev)
     alpha         = oev / (oev+prior_var_ct); alpha = alpha**0.5
-    dy            = post_mean_ct + alpha*( y_sim - prior_mean_ct ) - y_sim
+    dy            = post_mean_ct + alpha*( y - prior_mean_ct ) - y
 
-    A       = np.cov(x, y_sim)
+    A       = np.cov(x, y)
     covars  = A.at[:-1, -1].get()
     rr      = covars / prior_var_ct
     dx      = np.dot( np.expand_dims(rr, -1), np.expand_dims(dy, 0))
-    xpost   = x     + dx
-    obspost = y_sim + dy
+    xpost   = x + dx
+    obspost = y + dy
 
     if return_gain:
         return xpost, obspost, dx
